4913b9295355201163f199701c499cff90757c67
Max Wolffe <max.alan.wolffe@gmail.com>
2016-07-27 16:19:39 -0700

    Add Nomination form test

--- spec/features/sanity_spec.rb
+++ spec/features/sanity_spec.rb
@@ -2,10 +2,15 @@ require "rails_helper"
 
 RSpec.describe "The app works" do
   describe "if the home page" do
-    before { visit root_path }
+    before :each do
+      @current_event = Event.create(theme: "Test",
+                              description: "Test",
+                              date: "5-1-2016",)
+      visit root_path
+    end
 
     it "works" do
-      expect(page).to have_content "TEDxBerkeley"
+      expect(page).to have_content "Buy Tickets Now!"
     end
   end
 end
